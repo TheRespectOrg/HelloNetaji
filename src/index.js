b3493cfe225c2c0da25cbcd96dffc928671959a5
Zameer Ansari <zameer.ansari@instarem.com>
2020-02-19 00:23:18 +0530

    fix

--- src/index.js
+++ src/index.js
@@ -4,18 +4,22 @@ const path = require('path');
 const app = express();
 
 // An api endpoint that returns a short list of items
-app.get('/api/politicians', (req,res) => {
-    
-const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://${process.env.MONGODB_ATLAS_USERNAME}:${process.env.MONGODB_ATLAS_PASSWORD}@cluster0-jbzhq.mongodb.net/politicians?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  console.log(collection);  
-  client.close();
-  res.status(200).send(collection);
-});
+app.get('/api/politicians', (req, res) => {
+
+  const MongoClient = require('mongodb').MongoClient;
+  const uri = `mongodb+srv://${process.env.MONGODB_ATLAS_USERNAME}:${process.env.MONGODB_ATLAS_PASSWORD}@cluster0-jbzhq.mongodb.net/politicians?retryWrites=true&w=majority`;
+  const client = new MongoClient(uri, { useNewUrlParser: true });
+  client.connect(err => {
+    client.db("politicians")
+      .collection("politicians")
+      .find({}, function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        client.close();
+      });
+    // perform actions on the collection object
+    res.status(200);
+  });
 
 });
 
@@ -23,8 +27,8 @@ client.connect(err => {
 app.use(express.static(path.join(__dirname, '../client')));
 
 // Handles any requests that don't match the ones above
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname, '../client/index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
 const port = process.env.PORT || 5000;
