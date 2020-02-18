fd3b854586dd66c73fd63b27392cd9ee62cea529
Zameer Ansari <zameer.ansari@instarem.com>
2020-02-19 01:34:12 +0530

    FE update

---
+++ client/build/precache-manifest.ca31929659b2462fe3483971d454032f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5563352ea248ec73694b7d32b4f6754",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0537bea03aaeaed73af0",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "9877ed0701b1e5501180",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0537bea03aaeaed73af0",
+    "url": "/static/js/2.8f6dcb61.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.8f6dcb61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9877ed0701b1e5501180",
+    "url": "/static/js/main.10ccacae.chunk.js"
+  },
+  {
+    "revision": "d98d87b7717c2d361311",
+    "url": "/static/js/runtime-main.ccf050d1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
