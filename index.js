13129d68d9f6b94867405dec0391e8aa4a6718e7
Zameer Ansari <zameer.ansari@instarem.com>
2020-02-18 23:31:10 +0530

    fix

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'HelloNetajiFE/build')));
 
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname, '/HelloNetajiFE/build/index.html'));
+    res.sendFile(path.join(__dirname, 'HelloNetajiFE/build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
