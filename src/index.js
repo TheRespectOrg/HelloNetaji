381f504a544516dc865310169aab8744b417a01c
Zameer Ansari <zameer.ansari@instarem.com>
2020-02-24 02:54:43 +0530

    petitions++

--- src/index.js
+++ src/index.js
@@ -3,15 +3,53 @@ const path = require('path');
 require('dotenv').config();
 const app = express();
 const cors = require('cors');
-
+const bodyParser = require('body-parser')
 app.use(cors());
 
-// An api endpoint that returns a short list of items
-app.get('/api/politicians', (req, res) => {
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 
+const getMongoClient = () => {
   const MongoClient = require('mongodb').MongoClient;
   const uri = `mongodb+srv://${process.env.MONGODB_ATLAS_USERNAME}:${process.env.MONGODB_ATLAS_PASSWORD}@cluster0-jbzhq.mongodb.net/politicians?retryWrites=true&w=majority`;
   const client = new MongoClient(uri, { useNewUrlParser: true });
+  return client;
+}
+
+// An api endpoint for saving petitions data
+app.post('/api/petitions', (req, res) => {
+  var petition = {
+    Name: req.body.Name,
+    Email: req.body.Email,
+    Action: req.body.Action,
+    ActionArea: req.body.ActionArea,
+    Sections: req.body.Sections,
+    CaseConsent: req.body.CaseConsent,
+    PinCode: req.body.PinCode,
+    Phone: req.body.Phone,
+    Address: req.body.Address
+  };
+
+  
+  const client = getMongoClient();
+
+  client.connect(err => {
+    client.db("petitions")
+      .collection("petitions")
+      .insertOne(petition, function (err, res) {
+        if (err) throw err;
+        console.log("1 document inserted");
+        client.close();
+      });
+  });
+});
+
+// An api endpoint that returns a short list of items
+app.get('/api/politicians', (req, res) => {
+  const client = getMongoClient();
+
   client.connect(err => {
     client.db("politicians")
       .collection("politicians")
@@ -23,12 +61,13 @@ app.get('/api/politicians', (req, res) => {
         console.log(docs);
 
         // Resolve (or fulfill) the promise with data
+        client.close();
+
         res.status(200).send(docs);
       });
     // , function (err, result) {
     //   if (err) throw err;
     //   console.log(result);
-    //   client.close();
     //   // perform actions on the collection object
     //   res.status(200).send("test");
 
@@ -48,4 +87,4 @@ app.get('*', (req, res) => {
 const port = process.env.PORT || 5000;
 app.listen(port);
 
-console.log('App is listening on port http://localhost:' + port);
+console.log(`App is listening on port http://localhost:${port}`);
